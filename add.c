fb3c1cb1c1a72e0e62da03ec952c7caa29e7d31a
sksubhani1382 <sksubhani1382@gmail.com>
2023-10-10 21:24:41 +0530

    add

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the values:");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("Addition of number %d:",c);
+    return 0;
+}
---
+++ add.py
@@ -0,0 +1,4 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter b number:"))
+c=a+b
+print(c)
